750356b00d7e629a148b76b0aa2f49fb5b91dc0a
Brandon Ketterman <brandonkett24@gmail.com>
2020-08-21 14:28:54 -0400

    Run Npm I

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,21 +1,17 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Employee = require ("./Employee");
 
-test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
-});
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber) {
+        super (name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
 
-test('getRole() should return "Manager"', () => {
-  const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
-});
+module.exports = Manager;
